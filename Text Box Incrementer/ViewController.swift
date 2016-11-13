3f8f1dbcff5780f718d8a18420742489aed8a2a9
Mark <Mark@marks-macbook.local>
2016-11-13 09:33:11 -0800

    Initial Commit

---
+++ Text Box Incrementer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Text Box Incrementer
+//
+//  Created by Mark Nolte on 11/13/16.
+//  Copyright © 2016 Mark Nolte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
