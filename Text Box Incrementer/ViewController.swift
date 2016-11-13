b7aac7edfae58e48b8dd58331633dddc7f8b6ebb
Mark <Mark@Marks-MacBook.local>
2016-11-13 10:58:20 -0800

    initial version

--- Text Box Incrementer/ViewController.swift
+++ Text Box Incrementer/ViewController.swift
@@ -10,14 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  
+  @IBOutlet weak var amountField: UITextField!
+  @IBOutlet weak var stepper: UIStepper!
+  @IBOutlet weak var stepperValue: UILabel!
+  
+  @IBAction func stepperValueChanged(_ sender: UIStepper) {
+    print("stepper pressed. value is \(stepper.value)")
+    amountField.text = String(describing: stepper.value)
+    stepperValue.text = String(describing: stepper.value)
+  }
+  
+
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+    
+    amountField.text = String(describing: stepper.value)
+    stepperValue.text = String(describing: stepper.value)
+    
   }
 
 
